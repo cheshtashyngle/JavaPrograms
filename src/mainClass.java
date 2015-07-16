43687e3756781efe440b84ac27b4cf7ebc341c45
chestha <chestha@INchestha.local>
2015-07-16 11:57:03 +0530

    Updated

--- src/mainClass.java
+++ src/mainClass.java
@@ -1,10 +1,12 @@
 /**
  * Created by chestha on 16/07/15.
  */
+import java.util.Scanner;
 public class mainClass
 {
     public static void main(String[] args)
     {
+        Scanner sc=new Scanner(System.in);
         direction n=new direction('N');
         direction s=new direction('S');
         direction e=new direction('E');
@@ -17,8 +19,10 @@ public class mainClass
         e.setLeft(n);
         s.setLeft(e);
         w.setLeft(s);
-        mars myMars=new mars(5,5);
-        rover rover1=new rover(1,2,n);
+        int marX=sc.nextInt();
+        int marY=sc.nextInt();
+        mars myMars=new mars(marX,marY);
+        rover rover1=new rover(sc.nextInt(),sc.nextInt(),n);
         rover1.performAction('L');
         rover1.performAction('M');
         rover1.performAction('L');
